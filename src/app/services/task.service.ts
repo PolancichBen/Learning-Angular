dd2c72819639248df709bc166c5f40fdd0af5705
Benjamin Polancich <benjamin.p@salesboomerang.com>
2022-05-09 09:40:36 -0700

    Implemented delete tasks functionality

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs'; 
+import { Observable } from 'rxjs';
 
 import { Task } from '../Task';
-import { TASKS } from '../mock-tasks';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
-  private apiUrl = 'http://localhost:5001/tasks'
+  private apiUrl = 'http://localhost:5001/tasks';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
   }
 }
