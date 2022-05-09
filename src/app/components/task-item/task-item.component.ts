139e649a5010a66f5a6539dfd4f0e953c9ccc07a
Benjamin Polancich <benjamin.p@salesboomerang.com>
2022-05-09 09:45:40 -0700

    Added dblclick toggle functionaltity

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -12,12 +12,17 @@ import { Task } from '../../Task';
 export class TaskItemComponent implements OnInit {
   @Input() task: Task;
   @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
   faTimes = faTimes;
 
   constructor() {}
 
   onDelete(task: Task) {
    this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task) {
+   this.onToggleReminder.emit(task);
   }
 
   ngOnInit(): void {}
